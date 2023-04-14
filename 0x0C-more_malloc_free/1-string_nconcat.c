b949ee10aa9b77318f2eb6e3847ac48c2ffa425e
Daniel Obisanya <ifeoluwadaniel25@gmail.com>
2023-04-14 16:51:57 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int 1s1, 1s2, 1str, i;
+	unsigned int ls1, ls2, lstr, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -23,27 +23,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 
-	for (1s1 = 0; s1[1s1] != '\0'; 1s1++)
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
 
-	for (1s2 = 0; s2[1s2] != '\0'; 1s2++)
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
 
-	if (n > 1s2)
-		n = 1s2;
+	if (n > ls2)
+		n = ls2;
 
-	1str = 1s1 + n;
+	lstr = ls1 + n;
 
-	str = malloc(1str + 1);
+	str = malloc(lstr + 1);
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < 1str; i++)
-		if (i < 1s1)
+	for (i = 0; i < lstr; i++)
+		if (i < ls1)
 			str[i] = s1[i];
 		else
-			str[i] = s2[i - 1s1];
+			str[i] = s2[i - ls1];
 
 	str[i] = '\0';
 
