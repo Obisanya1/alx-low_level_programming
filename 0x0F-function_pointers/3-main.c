99accbd05d8f7b537700b6d6e912e56e17634f3d
Daniel Obisanya <ifeoluwadaniel25@gmail.com>
2023-04-21 05:27:53 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - prints result of operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0) if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98)
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
